c1196f06efe9cf452fca41964d2f1d7ade1eb586
chpeters <chpeters13@gmail.com>
2015-01-24 03:40:06 -0500

    Solution shell

---
+++ LocationPuzzleGame/Solution.swift
@@ -0,0 +1,26 @@
+//
+//  Solution.swift
+//  LocationPuzzleGame
+//
+//  Created by Charlie Peters on 1/24/15.
+//  Copyright (c) 2015 Jack Frysinger. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Solution {
+    var location : CLLocation
+    let answer : String
+    let manager = CLLocationManager()
+    
+    init(location : CLLocation, answer : String) {
+        self.location = location
+        self.answer = answer
+    }
+    
+    
+    
+    
+}
+    
